9cc3cf99d1668ae8802e344488d993222a321d22
Danylo <135544115+rainecarnation@users.noreply.github.com>
2023-06-04 21:19:23 +0300

    Added code to 1lab.py

--- 1lab.py
+++ 1lab.py
@@ -1,1 +1,13 @@
+def summ(x1, y1):
+    summ = x1 + y1
+    return summ
+x1=6
+y1=4
+res = summ
+print(res)
 
+def mult(x1, y1):
+    mult = x1 * y1
+    return mult
+res1 = mult(x1, y1)
+print(res1)
